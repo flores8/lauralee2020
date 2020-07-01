e83e7cd788b41540203cbefacbfd0c881b1d81e9
Lauralee Flores <flores.lauralee@gmail.com>
2020-07-01 15:30:28 -0700

    update the success page

--- src/pages/success.js
+++ src/pages/success.js
@@ -6,12 +6,21 @@ const IndexPage = () => {
   return (
     <Layout>
       <SEO title="Success!" />
-      <div class="content-container text">
+      <div class="content-container--narrow text">
         <h1>You're in!</h1>
-        <p>
+        <p class="text--sans">
           Welcome, you're officially on the list! You'll need to confirm your
-          email. Once it's confirmed you'll hear from when content is available.
+          email. Once it's confirmed you'll hear from me when content is
+          available.
         </p>
+        <p class="text--sans">
+          Normally you would see this on cssacademy.com and soon it will be
+          there. But, I'm still working on designing and building it.{" "}
+          <span role="img" aria-label="writing some code">
+            👩‍💻
+          </span>
+        </p>
+        <p class="text-sans">I'll keep you updated as I make progress.</p>
       </div>
     </Layout>
   )
